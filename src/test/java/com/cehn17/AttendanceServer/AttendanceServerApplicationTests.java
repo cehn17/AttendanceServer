71d3ba7d65d1946e2e0cc3179f286b5858184a14
Cesar Niveyro <cehn17@gmail.com>
2025-10-16 22:14:32 -0300

    ADD: configuración del proyecto en spring

---
+++ src/test/java/com/cehn17/AttendanceServer/AttendanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cehn17.AttendanceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
