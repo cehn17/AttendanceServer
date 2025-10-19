eb4ac534da62390ffcffe839ee2c2fa7092f0c34
Cesar Niveyro <cehn17@gmail.com>
2025-10-19 17:56:18 -0300

    ADD: Create Get Projects API

--- src/main/java/com/cehn17/AttendanceServer/service/ProjectService.java
+++ src/main/java/com/cehn17/AttendanceServer/service/ProjectService.java
@@ -6,6 +6,9 @@ import com.cehn17.AttendanceServer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProjectService {
 
@@ -18,5 +21,9 @@ public class ProjectService {
         project.setDuration(dto.getDuration());
         project.setStartDate(dto.getStartDate());
         return projectRepository.save(project).getDto();
+    }
+
+    public List<ProjectDTO> getAllProjects(){
+        return projectRepository.findAll().stream().map(Project::getDto).collect(Collectors.toList());
     }
 }
